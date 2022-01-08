b45a76d665fdfe9c676ef0ade466d6fff8d08715
Priyank <patelpriyank177@gmail.com>
2022-01-08 14:45:59 -0500

    added image

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div class = "card"> 
+      <div class ="cube">
+        <img src="/assets/image-equilibrium.jpg"  />
+      </div>
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .card {
+   
+ }
+</style>
